a96cca4daaad4273e35a26b873c3f62d255fdd0c
YuliaSincere <60033205+YuliaSincere@users.noreply.github.com>
2020-12-06 17:35:01 +0400

    Добавлен запуск

--- musicServiceServer/Startup.cs
+++ musicServiceServer/Startup.cs
@@ -1,19 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http.Connections;
-using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using musicServiceServer.Database;
 using MusicServiceServer.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace musicService
 {
